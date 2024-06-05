0a583d096b483133b29fc4c0ea94dc89c52071aa
Brian Nunez <bnunez@Brians-MBP.lan>
2024-06-05 01:01:54 -0400

    Update database configuration to use ngrok

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.CLEARDB_DATABASE_URL) {
-  const dbUrl = process.env.CLEARDB_DATABASE_URL;
-  const dbOptions = dbUrl.match(/mysql:\/\/([^:]+):([^@]+)@([^\/]+)\/(.+)\?/);
-  
-  sequelize = new Sequelize(dbOptions[4], dbOptions[1], dbOptions[2], {
-    host: dbOptions[3],
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306
-  });
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.DB_PORT || 3306
-    }
-  );
-}
+    port: process.env.DB_PORT || 3306
+  }
+);
 
 module.exports = sequelize;
